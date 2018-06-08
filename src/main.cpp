761726ffc25be2851e74dc95fb702ad145272818
Danylo Lubenets <ext-danylo.lubenets@here.com>
2018-06-08 20:10:02 +0300

    TicTacToe Ultimate pure Monte Carlo Tree Search

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,550 @@
-#include "TicTacToeGameAi.h"
-
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cmath>
 #include <ctime>
+#include <functional>
 #include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+namespace game
+{
+static const int SMALL_BRD_SZ = 3;
+
+double const SQRT_OF_TWO = sqrt( 2 );
+
+using AvailableCells = std::vector< std::vector< bool > >;
+
+struct MctsNode;
+using MctsNodePtr = std::shared_ptr< MctsNode >;
+using Children = std::vector< MctsNodePtr >;
+using ChildrenPtr = std::unique_ptr< Children >;
+
+template < typename T >
+using Board = std::vector< std::vector< T > >;
+
+struct Cell
+{
+    int row;
+    int col;
+};
+
+bool
+operator==( const Cell& rhs, const Cell& lhs )
+{
+    return ( rhs.row == lhs.row ) && ( rhs.col == lhs.col );
+}
+
+using Moves = std::vector< Cell >;
+
+enum class CellState
+{
+    e_Available,
+    e_Opponent,
+    e_Mine,
+    e_Common
+};
+
+enum class Result
+{
+    e_Result_Draw = 0,
+    e_Result_Miss,
+    e_Result_Hit,
+    e_Result_NotFinished,
+};
+
+Result
+game_state( Board< CellState > const& brd )
+{
+    auto const sz = brd.size( );
+    assert( sz == 3 );
+    assert( brd[ 0 ].size( ) == 3 );
+
+    auto cnt_available = 0;
+
+    // Check rows and cols
+    for ( size_t i = 0; i < sz; ++i )
+    {
+        auto cnt_row_mine = 0;
+        auto cnt_row_opponent = 0;
+        auto cnt_col_mine = 0;
+        auto cnt_col_opponent = 0;
+        for ( size_t j = 0; j < sz; ++j )
+        {
+            // Row
+            switch ( brd[ i ][ j ] )
+            {
+            case CellState::e_Mine:
+                ++cnt_row_mine;
+                break;
+            case CellState::e_Opponent:
+                ++cnt_row_opponent;
+                break;
+            case CellState::e_Available:
+                ++cnt_available;
+                break;
+            }
+            // Col
+            switch ( brd[ j ][ i ] )
+            {
+            case CellState::e_Mine:
+                ++cnt_col_mine;
+                break;
+            case CellState::e_Opponent:
+                ++cnt_col_opponent;
+                break;
+            }
+        }
+
+        if ( cnt_row_mine == sz || cnt_col_mine == sz )
+        {
+            return Result::e_Result_Hit;
+        }
+
+        if ( cnt_row_opponent == sz || cnt_col_opponent == sz )
+        {
+            return Result::e_Result_Miss;
+        }
+    }
+
+    // Check diagonals
+    auto cnt_diagonal1_mine = 0;
+    auto cnt_diagonal1_opponent = 0;
+    auto cnt_diagonal2_mine = 0;
+    auto cnt_diagonal2_opponent = 0;
+
+    for ( size_t i = 0; i < sz; ++i )
+    {
+        switch ( brd[ i ][ i ] )
+        {
+        case CellState::e_Mine:
+            ++cnt_diagonal1_mine;
+            break;
+        case CellState::e_Opponent:
+            ++cnt_diagonal1_opponent;
+            break;
+        }
+        switch ( brd[ i ][ sz - i - 1 ] )
+        {
+        case CellState::e_Mine:
+            ++cnt_diagonal2_mine;
+            break;
+        case CellState::e_Opponent:
+            ++cnt_diagonal2_opponent;
+            break;
+        }
+    }
+
+    if ( cnt_diagonal1_mine == sz || cnt_diagonal2_mine == sz )
+    {
+        return Result::e_Result_Hit;
+    }
+
+    if ( cnt_diagonal1_opponent == sz || cnt_diagonal2_opponent == sz )
+    {
+        return Result::e_Result_Miss;
+    }
+
+    return cnt_available > 0 ? Result::e_Result_NotFinished : Result::e_Result_Draw;
+}
+
+Cell
+get_board_coord( const Cell& cell )
+{
+    return {cell.row / SMALL_BRD_SZ, cell.col / SMALL_BRD_SZ};
+}
+
+Cell
+get_cell_coord( const Cell& cell )
+{
+    return {cell.row % SMALL_BRD_SZ, cell.col % SMALL_BRD_SZ};
+}
+
+struct MctsState
+{
+    MctsState( Board< Board< CellState > > board, bool turn, Cell last_move = {-1, -1} )
+        : m_board( board )
+        , m_big_board_states( SMALL_BRD_SZ,
+                              std::vector< CellState >( SMALL_BRD_SZ, CellState::e_Available ) )
+        , m_turn( turn )
+        , m_last_move( last_move )
+    {
+    }
+
+    Result
+    simulate( ) const
+    {
+        auto temp_state = std::unique_ptr< MctsState >( new MctsState( *this ) );
+
+        Result result = Result::e_Result_NotFinished;
+
+        while ( ( result = game::game_state( temp_state->m_big_board_states ) )
+                == Result::e_Result_NotFinished )
+        {
+            auto const possible_moves = temp_state->get_possible_moves( );
+
+            temp_state->play_move( possible_moves[ rand( ) % possible_moves.size( ) ] );
+        }
+
+        return result;
+    }
+
+    ChildrenPtr
+    get_children( MctsNodePtr parent ) const
+    {
+        auto possible_moves = get_possible_moves( );
+
+        auto possible_children = ChildrenPtr( new Children( ) );
+        possible_children->reserve( possible_moves.size( ) );
+
+        for ( auto const& possible_move : possible_moves )
+        {
+            auto temp_state = std::unique_ptr< MctsState >( new MctsState( *this ) );
+            temp_state->play_move( possible_move );
+            possible_children->push_back(
+                std::make_shared< MctsNode >( std::move( temp_state ), parent ) );
+        }
+
+        return std::move( possible_children );
+    }
+
+    const Cell&
+    get_last_move( ) const
+    {
+        return m_last_move;
+    }
+
+private:
+    Result
+    game_state( ) const
+    {
+        return game::game_state( m_big_board_states );
+    }
+
+    Moves
+    get_possible_moves( ) const
+    {
+        // Last cell position on the small board = next board
+        auto const target_board_coord
+            = m_last_move.row < 0 ? m_last_move : get_cell_coord( m_last_move );
+
+        Moves possible_moves;
+
+        if ( target_board_coord.row < 0
+             || m_big_board_states[ target_board_coord.row ][ target_board_coord.col ]
+                    != CellState::e_Available )
+        {
+            // all boards are possible
+            for ( size_t i = 0; i < SMALL_BRD_SZ; ++i )
+            {
+                for ( size_t j = 0; j < SMALL_BRD_SZ; ++j )
+                {
+                    if ( m_big_board_states[ i ][ j ] == CellState::e_Available )
+                    {
+                        append_board_moves( possible_moves, i, j );
+                    }
+                }
+            }
+        }
+        else
+        {
+            append_board_moves( possible_moves, target_board_coord.row, target_board_coord.col );
+        }
+
+        return possible_moves;
+    }
+
+    void
+    play_move( const Cell& cell )
+    {
+        auto const target_board_coord = get_board_coord( cell );
+        auto& target_board = m_board[ target_board_coord.row ][ target_board_coord.col ];
+
+        auto const row_cell = cell.row - ( target_board_coord.row * SMALL_BRD_SZ );
+        auto const col_cell = cell.col - ( target_board_coord.col * SMALL_BRD_SZ );
+        auto& target_cell = target_board[ row_cell ][ col_cell ];
+
+        target_cell = m_turn ? CellState::e_Mine : CellState::e_Opponent;
+        auto target_board_result = game::game_state( target_board );
+        if ( target_board_result != Result::e_Result_NotFinished )
+        {
+            m_big_board_states[ target_board_coord.row ][ target_board_coord.col ]
+                = target_board_result == Result::e_Result_Draw ? CellState::e_Common : target_cell;
+        }
+        m_last_move = cell;
+        m_turn = !m_turn;
+    }
+
+    void
+    append_board_moves( Moves& moves, size_t row, size_t col ) const
+    {
+        auto const& brd = m_board[ row ][ col ];
+        int const row_offset = SMALL_BRD_SZ * row;
+        int const col_offset = SMALL_BRD_SZ * col;
+
+        for ( int i = 0; i < SMALL_BRD_SZ; ++i )
+        {
+            for ( int j = 0; j < SMALL_BRD_SZ; ++j )
+            {
+                if ( brd[ i ][ j ] == CellState::e_Available )
+                {
+                    moves.push_back( {row_offset + i, col_offset + j} );
+                }
+            }
+        }
+    }
+
+private:
+    Board< Board< CellState > > m_board;
+    Board< CellState > m_big_board_states;
+    bool m_turn;
+    Cell m_last_move;
+};
+
+struct MctsNode : public std::enable_shared_from_this< MctsNode >
+{
+    explicit MctsNode( std::unique_ptr< MctsState > state, MctsNodePtr parent = nullptr )
+        : m_state( std::move( state ) )
+        , m_parent( parent )
+        , m_hits( 0 )
+        , m_misses( 0 )
+        , m_total_trials( 0 )
+    {
+    }
+
+    MctsNodePtr
+    choose_child( )
+    {
+        if ( !m_children )
+        {
+            m_children = m_state->get_children( shared_from_this( ) );
+        }
+
+        if ( m_children->empty( ) )
+        {
+            run_simulation( );
+        }
+        else
+        {
+            return explore_and_exploit( );
+        }
+
+        return nullptr;
+    }
+
+    MctsState const&
+    get_state( ) const
+    {
+        return *m_state;
+    }
+
+    MctsNodePtr
+    find_child( std::function< bool( const MctsState& ) > predicate ) const
+    {
+        if ( m_children )
+        {
+            for ( auto& child : *m_children )
+            {
+                if ( predicate( *( child->m_state ) ) )
+                {
+                    return child;
+                }
+            }
+        }
+
+        assert( false );
+        std::cerr << "MctsNode::find_child(): return nullptr" << std::endl;
+        return nullptr;
+    }
+
+private:
+    void
+    run_simulation( )
+    {
+        back_propagate( m_state->simulate( ) );
+    }
+
+    MctsNodePtr
+    explore_and_exploit( )
+    {
+        Children unexplored;
+        std::copy_if( m_children->begin( ), m_children->end( ), std::back_inserter( unexplored ),
+                      []( MctsNodePtr node ) { return node->m_total_trials == 0; } );
+
+        if ( !unexplored.empty( ) )
+        {
+            auto random_child = unexplored[ rand( ) % unexplored.size( ) ];
+            random_child->run_simulation( );
+            return random_child;
+        }
+        else
+        {
+            MctsNodePtr best_child;
+            auto best_potential = std::numeric_limits< double >::min( );
+            for ( auto child : *m_children )
+            {
+                auto const potential = child_potential( *child );
+                if ( potential > best_potential )
+                {
+                    best_potential = potential;
+                    best_child = child;
+                }
+            }
+            best_child->choose_child( );
+            return best_child;
+        }
+    }
+
+    void
+    back_propagate( Result result )
+    {
+        if ( result == Result::e_Result_Hit )
+        {
+            ++m_hits;
+        }
+        else if ( result == Result::e_Result_Miss )
+        {
+            ++m_misses;
+        }
+        ++m_total_trials;
+
+        auto parent_strong_ref = m_parent.lock( );
+        if ( parent_strong_ref )
+        {
+            parent_strong_ref->back_propagate( result );
+        }
+    }
+
+    double
+    child_potential( const MctsNode& child ) const
+    {
+        auto const w = child.m_hits + ( child.m_total_trials - child.m_hits - child.m_misses ) / 2;
+        auto const n = child.m_total_trials;
+        auto const c = SQRT_OF_TWO;
+        auto const t = m_total_trials;
+        return double( w ) / n + c * sqrt( log( double( t ) ) / n );
+    }
+
+private:
+    std::unique_ptr< MctsState > m_state;
+    std::weak_ptr< MctsNode > m_parent;
+
+    int m_hits;
+    int m_misses;
+    int m_total_trials;
+
+    ChildrenPtr m_children;
+};
+
+struct TicTacToeGameAI
+{
+    explicit TicTacToeGameAI( const Cell& opponent_move = {-1, -1} )
+    {
+        Board< Board< CellState > > board(
+            SMALL_BRD_SZ,
+            std::vector< Board< CellState > >(
+                SMALL_BRD_SZ,
+                Board< CellState >( SMALL_BRD_SZ, std::vector< CellState >(
+                                                      SMALL_BRD_SZ, CellState::e_Available ) ) ) );
+
+        if ( opponent_move.row >= 0 )
+        {
+            auto const board_coord = game::get_board_coord( opponent_move );
+            auto const cell_coord = game::get_cell_coord( opponent_move );
+            board[ board_coord.row ][ board_coord.col ][ cell_coord.row ][ cell_coord.col ]
+                = CellState::e_Opponent;
+        }
+
+        std::unique_ptr< MctsState > state(
+            new MctsState( std::move( board ), true, opponent_move ) );
+        m_tree = std::make_shared< MctsNode >( std::move( state ) );
+
+        auto const end_time = std::chrono::duration_cast< std::chrono::milliseconds >(
+                                  std::chrono::system_clock::now( ).time_since_epoch( ) )
+                              + std::chrono::milliseconds( 995 );
+
+        make_move( m_tree, end_time );
+    }
+
+    void
+    opponent_move( const Cell& position )
+    {
+        auto current_node_strong_ref = m_current_node.lock( );
+
+        // Ensure current node has children
+        current_node_strong_ref->choose_child( );
+
+        // Get opponent child
+        auto opponent_child = current_node_strong_ref->find_child(
+            [&position]( const MctsState& state ) { return state.get_last_move( ) == position; } );
+
+        auto const end_time = std::chrono::duration_cast< std::chrono::milliseconds >(
+                                  std::chrono::system_clock::now( ).time_since_epoch( ) )
+                              + std::chrono::milliseconds( 95 );
+        make_move( opponent_child, end_time );
+    }
+
+    Cell
+    get_ai_move( ) const
+    {
+        return m_current_node.lock( )->get_state( ).get_last_move( );
+    }
+
+private:
+    void
+    make_move( MctsNodePtr node, std::chrono::milliseconds end_time )
+    {
+        auto current_time = std::chrono::duration_cast< std::chrono::milliseconds >(
+            std::chrono::system_clock::now( ).time_since_epoch( ) );
+        while ( current_time < end_time )
+        {
+            m_current_node = node->choose_child( );
+            current_time = std::chrono::duration_cast< std::chrono::milliseconds >(
+                std::chrono::system_clock::now( ).time_since_epoch( ) );
+        }
+    }
+
+private:
+    MctsNodePtr m_tree;
+    std::weak_ptr< MctsNode > m_current_node;
+};
+}  // namespace game
 
 namespace
 {
-// const bool srand_init = []( ) {
-//     srand( static_cast< unsigned int >( time( NULL ) ) );
-//     return true;
-// }( );
+const bool srand_init = []( ) {
+    srand( static_cast< unsigned int >( time( NULL ) ) );
+    return true;
+}( );
 }
 
 using Brd = std::vector< std::vector< char > >;
 
-const size_t SMALL_BOARD_SIZE = mcts::TicTacToeGameAI::SMALL_BOARD_SIZE;
-const size_t BIG_BOARD_SIZE = mcts::TicTacToeGameAI::BIG_BOARD_SIZE;
+const size_t SMALL_BRD_SZ = game::SMALL_BRD_SZ;
+const size_t BIG_BRD_SIZE = SMALL_BRD_SZ * SMALL_BRD_SZ;
 
 void
 print_board( const Brd& brd )
 {
-    auto const big = brd.size( ) > SMALL_BOARD_SIZE;
+    auto const big = brd.size( ) > SMALL_BRD_SZ;
     auto const header_footer = big ? "#############" : "#####";
 
     std::cout << header_footer << std::endl;
-    for ( size_t i = 0; i < ( big ? BIG_BOARD_SIZE : SMALL_BOARD_SIZE ); ++i )
+    for ( size_t i = 0; i < ( big ? BIG_BRD_SIZE : SMALL_BRD_SZ ); ++i )
     {
         std::cout << "#";
-        for ( size_t j = 0; j < ( big ? BIG_BOARD_SIZE : SMALL_BOARD_SIZE ); ++j )
+        for ( size_t j = 0; j < ( big ? BIG_BRD_SIZE : SMALL_BRD_SZ ); ++j )
         {
             std::cout << brd[ i ][ j ];
-            if ( ( j + 1 ) % SMALL_BOARD_SIZE == 0 )
+            if ( ( j + 1 ) % SMALL_BRD_SZ == 0 )
             {
                 std::cout << '#';
             }
         }
-        if ( ( i + 1 ) % SMALL_BOARD_SIZE == 0 )
+        if ( ( i + 1 ) % SMALL_BRD_SZ == 0 )
         {
             std::cout << "\n" << header_footer;
         }
@@ -45,14 +555,15 @@ print_board( const Brd& brd )
 int
 main( )
 {
-    auto const board_size = BIG_BOARD_SIZE;
+    Brd visualization( BIG_BRD_SIZE, std::vector< char >( BIG_BRD_SIZE, '-' ) );
 
-    Brd visualization( board_size, std::vector< char >( board_size, '-' ) );
+    game::Cell pos = {1, 1};
+    if ( pos.row >= 0 )
+    {
+        visualization[ pos.row ][ pos.col ] = '0';
+    }
 
-    std::vector< std::vector< bool > > available( board_size,
-                                                  std::vector< bool >( board_size, true ) );
-
-    auto game = std::make_shared< mcts::TicTacToeGameAI >( available, 100 );
+    auto game = std::make_shared< game::TicTacToeGameAI >( pos );
 
     bool cont = true;
     while ( cont )
@@ -61,7 +572,6 @@ main( )
         visualization[ ai_move.row ][ ai_move.col ] = 'X';
         print_board( visualization );
 
-        mcts::MovePosition pos;
         std::cin >> pos.row >> pos.col;
         visualization[ pos.row ][ pos.col ] = '0';
         game->opponent_move( pos );
